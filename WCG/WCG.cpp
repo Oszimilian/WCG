b3d3302a1a1d81d927574caab6d0b1b79517029b
Oszimilian <maximilian@hoffmann-maximilian.net>
2023-09-23 22:16:38 +0200

    Projektdateien hinzufügen.

---
+++ WCG/WCG.cpp
@@ -0,0 +1,12 @@
+﻿// WCG.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "WCG.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ WCG/WCG.h
@@ -0,0 +1,8 @@
+﻿// WCG.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
